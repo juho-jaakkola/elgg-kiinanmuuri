339b0d8dd839cd2b1b335be04cf1ec7cc2887fae
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-04-12 23:54:00 +0300

    Changed the email address.

--- languages/fi.php
+++ languages/fi.php
@@ -1,7 +1,7 @@
 <?php
 
 $finnish = array(
-	'walled_garden:info' => 'Jos rekisteröitymisessä tai kirjautumisessa on ongelmia, voit pyytää apua lähettämällä sähköpostia osoitteeseen juho_j(a)hotmail.com',
+	'walled_garden:info' => 'Jos rekisteröitymisessä tai kirjautumisessa on ongelmia, voit pyytää apua lähettämällä sähköpostia osoitteeseen info(a)kiinanmuuri.org',
 );
 
 add_translation('fi', $finnish);
